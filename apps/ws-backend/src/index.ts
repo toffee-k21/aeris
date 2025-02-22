19924d736f3f60254ef59474927757a29fcbdf65
Taufiq Hassan <taufiq9.2004@gmail.com>
2025-02-22 20:54:27 +0530

    wriiten logic for ws basic setup

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,12 +1,34 @@
-// import {JWT_SECRET} from "@repo/common-backend/config"
+import {JWT_SECRET} from "@repo/common-backend/config"
+import JWT, {JwtPayload} from "jsonwebtoken"
 // console.log(JWT_SECRET);
 
 import { WebSocketServer } from 'ws';
 
+interface CustomJwtPayload extends JwtPayload {
+userId : string
+}
+
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+function checkUserAuth (token:string):string | null {
+  const resp = JWT.verify(token,JWT_SECRET) as CustomJwtPayload;
+  if(!resp?.userId){
+    return null;
+  }
+  return resp.userId;
+}
+
+wss.on('connection', function connection(ws, request) {
   ws.on('error', console.error);
+
+  const url = request.url;
+  if(!url){
+    return;
+  }
+
+  const queryParam = new URLSearchParams(url.split("?")[1]);
+  const token = queryParam.get('token') || "";
+  const userId = checkUserAuth(token);
 
   ws.on('message', function message(data) {
     console.log('received: %s', data);

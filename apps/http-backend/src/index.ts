e91f04ca3ca5db6be2e13dc1b85f144e859ffdbd
Taufiq Hassan <taufiq9.2004@gmail.com>
2025-02-24 01:39:00 +0530

    added chat route

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {router as userRouter} from "./routes/user"
 import {router as roomRouter} from "./routes/room"
+import {router as chatRouter} from "./routes/chat"
 import { AuthMiddleware } from "./utils/authMiddleware"
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use("/user",userRouter);
 app.use("/room",AuthMiddleware,roomRouter);
+app.use("/chat",AuthMiddleware,chatRouter);
 
 app.listen(8000,()=>{
     console.log("server running on port 8000");

3889b029471a8295095a07b4a237d52b47f5c42a
Taufiq Hassan <taufiq2004.21@gmail.com>
2025-11-27 01:07:11 +0530

    tranforming

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+import WebSocket from "ws";
+export type Topic = string;
+export type Payload = any;
+
+export interface PublishMessage {
+  topic: Topic;
+  payload: Payload;
+}
+export interface ClientSubscriptions {
+  [topic: string]: true;
+}
+export interface WSClient extends WebSocket {
+  subscriptions?: Set<Topic>;
+}
+
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export function safeJSONParse(s: unknown) {
+  try { return JSON.parse(String(s)); } catch { return null; }
+}
+
